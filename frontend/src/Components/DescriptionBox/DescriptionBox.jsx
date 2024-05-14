2fae55bc71464135f8c352bd8f88bad8bf5bf5a3
Shivang309 <115680892+Shivang309@users.noreply.github.com>
2024-05-15 01:06:36 +0530

    First Commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+          Description
+        </div>
+        <div className="descriptionbox-nav-box faid">
+          Reviews(122)
+        </div>
+      </div>
+      <div className="descriptionbox-description">
+          <p>There's a lot of adjectives to describe clothes that you can learn, and knowing how to use them will ensure that, when it comes to describing what you want to look like, you can be really detailed and extensive. That's why it's valuable to learn these terms.</p>
+          <p>This article has compiled 100 different words you can use to describe clothes, and below the main list of 100 words we’ve explained our reasoning for a lot of the top picks from that very list, for your personal enjoyment.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
